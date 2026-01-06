5d296bce0d94ae95641bd4c8ad132c947a19444b
Gaurav Gaurav <zen@Zen-master.local>
2026-01-07 02:51:01 +0530

    Remove Nativefier configuration - revert to basic Flask app

--- run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""FRCR Examiner - Web App Launcher"""
-from app import app
-
-if __name__ == '__main__':
-    print("\n" + "="*60)
-    print("  FRCR Examiner is running at http://localhost:5000")
-    print("="*60 + "\n")
-    app.run(host='127.0.0.1', port=5000, debug=False)
